f2eadbe091099085e7b93e612bab2b2fd07ee8c1
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-04-12 12:46:02 +0200

    mysql dbs

--- model/database.js
+++ model/database.js
@@ -10,29 +10,32 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "mvp_dancing",
-  multipleStatements: true
+  database: DB_NAME || "yogaproject",
+  multipleStatements: true,
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists classes; CREATE TABLE classes (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, day varchar(255) NOT NULL, time TIME NOT NULL, address varchar(255) NOT NULL, price double NOT NULL, partner BOOLEAN NOT NULL,style INT NOT NULL, instructor INT NOT NULL, image varchar(255), PRIMARY KEY (id));";
+  let sql =
+    "DROP TABLE if exists classes; CREATE TABLE classes (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, day varchar(20) NOT NULL, time TIME NOT NULL, address varchar(255) NOT NULL, price doecimal NOT NULL, style INT NOT NULL, instructor INT NOT NULL, image varchar(255), PRIMARY KEY (id));";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table creation 'classes' was successful!");
     console.log("Closing...");
   });
 
-  sql="DROP TABLE if exists dance_styles; CREATE TABLE dance_styles (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, video_url varchar(255) NOT NULL, description TEXT NOT NULL, image varchar(255), PRIMARY KEY (id));";
+  sql =
+    "DROP TABLE if exists yoga_styles; CREATE TABLE yoga_styles (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, description TEXT NOT NULL, image varchar(255), PRIMARY KEY (id));";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table creation 'dance_styles' was successful!");
     console.log("Closing...");
   });
 
-  sql="DROP TABLE if exists instructors; CREATE TABLE instructors (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, introduction TEXT NOT NULL, email varchar(255) NOT NULL, photo varchar(255) NOT NULL, PRIMARY KEY (id));";
+  sql =
+    "DROP TABLE if exists instructors; CREATE TABLE instructors (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, introduction TEXT NOT NULL, email varchar(255) NOT NULL, photo varchar(255) NOT NULL, PRIMARY KEY (id));";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table creation 'instructors' was successful!");
@@ -41,5 +44,3 @@ con.connect(function(err) {
 
   con.end();
 });
-
-
