f4e2988e838ca1853e3a65590d5fc8a35215d01f
Beatrice Rossi <rossi.beatrice93@gmail.com>
2021-05-10 17:28:56 +0200

    started file upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var fileUpload = require("express-fileupload");
 
 var indexRouter = require("./routes/index");
 var yogaStylesRouter = require("./routes/yoga-styles");
@@ -15,10 +16,16 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./tmp/",
+  })
+);
 // app.use(express.static(path.join(__dirname, 'public')));
 // not too sure why - ask
 app.use(express.static(path.join(__dirname, "client/build")));
-app.use(express.static("public/images"));
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 app.use("/yogastyles", yogaStylesRouter);
--- routes/instructors.js
+++ routes/instructors.js
@@ -1,58 +1,71 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 const db = require("../model/helper");
+var fs = require("fs/promises");
+var path = require("path");
+const mime = require("mime-types");
+const { v4: uuidv4 } = require("uuid");
 
 router.use(bodyParser.json());
 
 // get everything from instructors
-router.get('/', async function(req, res) {
-  try{
+router.get("/", async function (req, res) {
+  try {
     const response = await db("SELECT * FROM instructors;");
     res.send(response.data);
-  }catch(err){
+  } catch (err) {
     res.send(err);
   }
 });
 
 // add a new instructor
-router.post('/', async function(req, res) {
+router.post("/", async function (req, res) {
   const name = req.body.name;
   const introduction = req.body.introduction;
   const email = req.body.email;
-  const photo = req.body.photo;
+  const imagefile = req.files;
+  const extension = mime.extension(imagefile.mimetype);
+  const filename = uuidv4() + "." + extension;
+  const tmp_path = imagefile.tempFilePath;
+  const target_path = path.join(__dirname, "../public/img") + filename;
 
-  try{
-    const response = await db(`INSERT INTO instructors (name, introduction, email, photo) VALUES ("${name}", "${introduction}", "${email}", "${photo}");`);
-    res.send({ message: "Instructor added successfully"});
-  }catch(err){
+  try {
+    await fs.rename(tmp_path, target_path);
+    await db(
+      `INSERT INTO instructors (name, introduction, email, photo) VALUES ("${name}", "${introduction}", "${email}", "${filename}");`
+    );
+    getImages(req, res);
+    res.send({ message: "Instructor added successfully" });
+  } catch (err) {
     res.send(err);
   }
 });
 
 // update an existing instructor
-router.put('/:id', async function(req, res) {
-  try{
+router.put("/:id", async function (req, res) {
+  try {
     const { id } = req.params;
     const currRecord = await db(`SELECT * FROM instructors WHERE id="${id}";`);
-    const newRecord = {...currRecord, ...req.body};
-    const response = await db(`UPDATE instructors SET name="${newRecord.name}", introduction="${newRecord.introduction}", email="${newRecord.email}", photo="${newRecord.photo}" WHERE id="${id}";`);
-    res.send({message: "Record updated successfully"});
-  } catch(err) {
+    const newRecord = { ...currRecord, ...req.body };
+    const response = await db(
+      `UPDATE instructors SET name="${newRecord.name}", introduction="${newRecord.introduction}", email="${newRecord.email}", photo="${newRecord.photo}" WHERE id="${id}";`
+    );
+    res.send({ message: "Record updated successfully" });
+  } catch (err) {
     res.send(err);
   }
-
-})
+});
 
 // delete an instructor
-router.delete('/:id', async function(req, res) {
+router.delete("/:id", async function (req, res) {
   const { id } = req.params;
-  try{
+  try {
     const response = await db(`DELETE FROM instructors WHERE id="${id}";`);
-    res.send({message: "Record deleted successfully."});
-  }catch(err){
+    res.send({ message: "Record deleted successfully." });
+  } catch (err) {
     res.send(err);
   }
-})
+});
 
 module.exports = router;
